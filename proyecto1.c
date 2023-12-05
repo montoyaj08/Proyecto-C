c1693d7329042172fdf96a60d66bbea7a04f941a
montoyaj08 <montoyajose902@gmail.com>
2023-12-05 17:59:01 -0600

    proyecto1.c funcionando SIN comentarios

--- proyecto1.c
+++ proyecto1.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
-         
-void funcion(int matrix, int num_ciudades, int actual, int destino, int camino[], int distancia, int visitados){
+#include <stdbool.h>
+
+int ciudad_visitada(int camino[], int visitados, int ciudad) {
+    for (int i = 0; i < visitados; i++) {
+        if (camino[i] == ciudad) {
+            return false; 
+        }
+    }
+    return true;
+}
+
+void caminos(int num_ciudades, int matrix[num_ciudades][num_ciudades], int actual, int destino, int camino[], int distancia, int visitados, int *punt_distancia, int min_camino[], int *punt_visitados){
 
     camino[visitados] = actual;
 
     if (actual == destino) {
-        printf("Encontrado un camino nuevo");
+        if (distancia < *punt_distancia) {
+            *punt_distancia = distancia;
+            *punt_visitados = visitados;
+            for (int i = 0; i <= visitados; i++) {
+                min_camino[i] = camino[i];
+            }
+        }
     } 
 
     else {
 
         for (int i = 0; i < num_ciudades; i++) {
-            if (matrix[actual][i] != 0 && matrix[actual][i] != -1) {
-                funcion(matrix, num_ciudades, i, destino, camino, distancia + matrix[actual][i], visitados + 1);
+            if (matrix[actual][i] != 0 && matrix[actual][i] != -1 && ciudad_visitada(camino, visitados, i)) {
+                caminos(num_ciudades, matrix, i, destino, camino, distancia + matrix[actual][i], visitados + 1, punt_distancia, min_camino, punt_visitados);
             }
         }
     }
 
-    camino[visitados] = 0;
+    visitados = visitados - 1;
+    distancia = distancia - matrix[camino[visitados - 1]][actual];
 }
 
 int main(){
+
     int num_ciudades;
-    printf("Ingrese en numero de ciudades: ");
+    printf("Ingrese el numero de ciudades: ");
     scanf("%d", &num_ciudades);
+
     int matrix[num_ciudades][num_ciudades];
     int distancia;
     for (int i = 1; i < num_ciudades; i++){
@@ -39,6 +58,7 @@ int main(){
         matrix[i][i] = 0;
     }
 
+// ****************************************************
     printf("Matriz:\n");
     for (int i = 0; i < num_ciudades; i++) {
         for (int j = 0; j < num_ciudades; j++) {
@@ -46,14 +66,27 @@ int main(){
         }
         printf("\n");
     }
-
-    int origen, destino, min_distancia;
-
+// ****************************************************
+    
+    int origen, destino;
     printf("Indique el origen y el destino: ");
-    scanf("%d %d", &origen, &destino);
+    scanf("%d", &origen);
+    scanf("%d", &destino);
 
-    int camino[];
-    funcion(matrix, num_ciudades, origen, destino, camino, 0, 0);
+    int camino[num_ciudades];
+    int mejor_distancia = 1000000;
+    int mejor_camino[num_ciudades];
+    int mejor_visitados = 0;
+    caminos(num_ciudades, matrix, origen, destino, camino, 0, 0, &mejor_distancia, mejor_camino, &mejor_visitados);
+
+    printf("Camino más corto: ");
+    for (int i = 0; i < mejor_visitados + 1; i++) {
+        printf("%d ", mejor_camino[i]);
+    }
+    printf("\n");
+
+    // Imprimir la distancia total del camino más corto
+    printf("Distancia total del camino más corto: %d\n", mejor_distancia);
 
     return 0;
 }
