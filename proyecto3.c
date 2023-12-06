6d094370486508933ac079d62eb3debbb5ff9bac
Susan13ma <147725812+Susan13ma@users.noreply.github.com>
2023-12-06 09:56:44 -0600

    Update proyecto3.c
    Se incluye descripcion y parte de integrantes

--- proyecto3.c
+++ proyecto3.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Integrantes:
+1.
+2.
+3. Susan Morales Alfaro, A94203
+
+Descripción: Se le solicita al usuario el número de filas y columnas desea para la
+matriz NxM. Luego, el usuario ingresa la cantidad de números enteros en cada fila y columna.
+Finalmente, la función devuelve al usuario las direcciones de memoria de los números
+que inician cada fila y, se libera la memoria.
+*/
+           
+
 // La función retorna un número entero: 0 si no hay errores, -1 si hubo algún error
 int obtenerDirecciones(int **matriz, int filas, int columnas, int ***direcciones) {
     if (matriz == NULL || direcciones == NULL) {
